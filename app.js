57dee20700f59cf6c3231a1ddb42b23326c94e5d
Shashi <rautelsi@mail.uc.edu>
2023-12-12 00:04:44 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+
+function login() {
+  // Your login logic here
+  console.log('Login function called');
+}
+
+function logout() {
+  // Your logout logic here
+  console.log('Logout function called');
+}
+
+// Other functions and logic
